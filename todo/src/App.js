4be0dfdd2e009cbc55265015f91af18228cf3f0d
michelle hey <miscmiche@gmail.com>
2020-06-17 21:36:42 -0400

    Formatting

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+
+import React, { useState, useReducer } from 'react';
+import { reducer, initialState } from './reducers/reducers';
+import TodoForm from './Components/TodoForm/TodoForm';
 import './App.css';
+import TodoList from './Components/TodoList/TodoList';
 
 function App() {
+  const [toDoList, dispatch] = useReducer(reducer, initialState);
+  const [newTodo, setNewTodo] = useState();
+
+  const handleChanges = event => {
+    setNewTodo(event.target.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    dispatch({ type: 'ADD_TODO', payload: newTodo })
+  }
+
+  const clearCompleted = () => {
+    dispatch({ type: 'CLEAR_COMPLETED' })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <h1>To Do Application Using React</h1>
+      <TodoForm 
+        handleChanges={handleChanges}
+        handleSubmit={handleSubmit}
+        clearCompleted={clearCompleted}
+      />
+      <TodoList 
+        toDoList={toDoList} 
+        dispatch={dispatch}
+      />
     </div>
   );
 }
